545e8e19ebc88ea4b12087fd361cd1f448ad520f
Arman <Arman.agora@gmail.com>
2020-03-09 21:31:53 +0100

    Ventana principal, Input Nº Valor y añadido Boton

--- app/src/main/java/com/juan/prohealth/MainActivity.kt
+++ app/src/main/java/com/juan/prohealth/MainActivity.kt
@@ -2,6 +2,7 @@ package com.juan.prohealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         tvInit.text = "Proyecto ProHealth V1"
+
+        btn_planificar.setOnClickListener {
+
+            if(!et_valor.text.toString().isNullOrEmpty()) {
+                val valor = Integer.parseInt(et_valor.text.toString())
+                if(valor > 0) {
+                    Toast.makeText(this, "El valor introducido es: " + et_valor.text, Toast.LENGTH_LONG).show()
+                    calcularTodo(valor)
+                }
+            }
+        }
+    }
+
+    fun calcularTodo(valor: Int) {
+        // TODO
     }
 }
