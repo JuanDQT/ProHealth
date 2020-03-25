856329a3948a38ecd7b0a1cf86d8540917f37e3f
Juan <juan@MacBook-Pro-de-Juan.local>
2020-03-25 21:19:46 +0100

    vista calendarip preparada #2

--- app/src/main/java/com/juan/prohealth/CalendarioActivity.kt
+++ app/src/main/java/com/juan/prohealth/CalendarioActivity.kt
@@ -1,12 +1,33 @@
 package com.juan.prohealth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jakewharton.threetenabp.AndroidThreeTen
+import kotlinx.android.synthetic.main.activity_calendario.*
+import java.util.*
+
 
 class CalendarioActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendario)
+
+        btnRandom.setOnClickListener {
+            // 1. Fecha que queremos mostrar
+            val c = Calendar.getInstance()
+            c.time = Date()
+            c.add(Calendar.DATE, (1..15).random())
+            val fechaRandom = c.time
+
+            // 2. Convertirla a long
+            //cvCalendario.date = fechaRandom.time
+
+            pintarMasDias()
+        }
+    }
+
+    fun pintarMasDias() {
+        //cvCalendario.
     }
 }
