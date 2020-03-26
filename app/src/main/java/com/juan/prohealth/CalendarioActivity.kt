dcfd0acdd62363d3d9686d9b1e71d4cf29464811
Juan <juan@MacBook-Pro-de-Juan.local>
2020-03-26 17:54:58 +0100

    Anadido calendario
    Anadido soporte al calendario.
    Ejemplo de prueba tambien anadido

--- app/src/main/java/com/juan/prohealth/CalendarioActivity.kt
+++ app/src/main/java/com/juan/prohealth/CalendarioActivity.kt
@@ -2,32 +2,44 @@ package com.juan.prohealth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.jakewharton.threetenabp.AndroidThreeTen
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import kotlinx.android.synthetic.main.activity_calendario.*
-import java.util.*
 
-
+// https://github.com/prolificinteractive/material-calendarview
 class CalendarioActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendario)
 
-        btnRandom.setOnClickListener {
-            // 1. Fecha que queremos mostrar
-            val c = Calendar.getInstance()
-            c.time = Date()
-            c.add(Calendar.DATE, (1..15).random())
-            val fechaRandom = c.time
+        btnPuntos.setOnClickListener {
+            pintarPuntos()
+        }
 
-            // 2. Convertirla a long
-            //cvCalendario.date = fechaRandom.time
-
-            pintarMasDias()
+        btnRango.setOnClickListener {
+            pintarRango()
         }
     }
 
-    fun pintarMasDias() {
-        //cvCalendario.
+    fun pintarPuntos() {
+        calendarView.clearSelection()
+        calendarView.selectionMode = MaterialCalendarView.SELECTION_MODE_MULTIPLE
+
+        // Itera 7 veces y por cada itearcion marca un dia aleatorio en el calendario
+        for (x in 0 until 7) {
+            //calendarView.selectedDate = CalendarDay.from(2020, 3, (1 until 31).random())
+            calendarView.setDateSelected(CalendarDay.from(2020, 3, (1 until 31).random()), true)
+        }
+
     }
+
+    // Coge una fecha aleatoria del mes actual, y marca en el calendario los dias siguientes(aleatorio 1 - 5)
+    fun pintarRango() {
+        calendarView.clearSelection()
+        calendarView.selectionMode = MaterialCalendarView.SELECTION_MODE_RANGE
+        val diaAleatorio = (1 until 25).random()
+        calendarView.selectRange(CalendarDay.from(2020,3, diaAleatorio), CalendarDay.from(2020,3, diaAleatorio + (1 until 5).random()))
+    }
+
 }
