0f0b85ea12a954cdc07dcbb188d012f6faa4f3d0
Juan <juan@MacBook-Pro-de-Juan.local>
2020-04-03 10:13:03 +0200

    open json

--- app/src/main/java/com/juan/prohealth/MainActivity.kt
+++ app/src/main/java/com/juan/prohealth/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.juan.prohealth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,5 +34,22 @@ class MainActivity : AppCompatActivity() {
 
     fun calcularTodo(valor: Int) {
         // TODO
+    }
+
+    fun openJSON(): String? {
+        var json: String? = null
+        json = try {
+            val inputStream: InputStream = com.juan.poesiadecine.Common.context.getAssets()
+                .open(com.juan.poesiadecine.Common.JSON_CONFIG)
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            String(buffer, UTF_8)
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            return null
+        }
+        return json
     }
 }
