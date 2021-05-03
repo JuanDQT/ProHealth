302871dd7bf9c55f5f6287f81d7a74bf52f7bc42
Arman <arman.agora@gmail.com>
2021-05-03 18:17:21 +0200

    Modified XML and CustomButton.Kt
    Se ha modificado la clase CustomButtom para reemplazarlo por binding.
    Se ha modificado el XML para que se pueda visualizar bien los botones en forma circular y renombrado los componentes.

--- app/src/main/java/com/juan/prohealth/custom/CustomButton.kt
+++ app/src/main/java/com/juan/prohealth/custom/CustomButton.kt
@@ -1,27 +1,40 @@
 package com.juan.prohealth.custom
 
 // https://www.vogella.com/tutorials/AndroidCustomViews/article.html
-import android.app.Service
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.RelativeLayout
 import com.juan.prohealth.R
 import com.juan.prohealth.alignTo
-import kotlinx.android.synthetic.main.custom_button.view.*
+import com.juan.prohealth.databinding.CustomButtonBinding
 
-class CustomButton @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : RelativeLayout(context, attrs, defStyleAttr) {
+class CustomButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : RelativeLayout(context, attrs, defStyleAttr) {
 
     init {
 
-        val inflater = context.getSystemService(Service.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        inflater.inflate(R.layout.custom_button, this)
+        val binding = CustomButtonBinding.inflate(LayoutInflater.from(context), this)
 
         attrs?.let {
-            val typedArray = context.obtainStyledAttributes(it, R.styleable.custom_button_attrs, 0, 0)
-            val title = resources.getText(typedArray .getResourceId(R.styleable.custom_button_attrs_cb_title, R.string.empty))
-            my_textview.text = title
-            my_iv.setBackgroundResource(typedArray .getResourceId(R.styleable.custom_button_attrs_cb_resource, R.string.empty))
+            val typedArray =
+                context.obtainStyledAttributes(it, R.styleable.custom_button_attrs, 0, 0)
+            val title = resources.getText(
+                typedArray.getResourceId(
+                    R.styleable.custom_button_attrs_cb_title,
+                    R.string.empty
+                )
+            )
+            binding.menuText.text = title
+            binding.iconMenu.setBackgroundResource(
+                typedArray.getResourceId(
+                    R.styleable.custom_button_attrs_cb_resource,
+                    R.string.empty
+                )
+            )
 
             if (typedArray.hasValue(R.styleable.custom_button_attrs_cb_icon_position)) {
                 when (typedArray.getString(R.styleable.custom_button_attrs_cb_icon_position)) {
@@ -29,13 +42,13 @@ class CustomButton @JvmOverloads constructor(context: Context, attrs: AttributeS
                         return@let
                     }
                     "1" -> {
-                        my_cv.alignTo(RelativeLayout.ALIGN_PARENT_END)
-                        my_textview.alignTo(RelativeLayout.ALIGN_PARENT_START)
-                        my_textview.alignTo(RelativeLayout.ALIGN_PARENT_END)
-                        val lpCIV = my_textview.layoutParams as RelativeLayout.LayoutParams
+                        binding.containIcon.alignTo(ALIGN_PARENT_END)
+                        binding.menuText.alignTo(ALIGN_PARENT_START)
+                        binding.menuText.alignTo(ALIGN_PARENT_END)
+                        val lpCIV = binding.menuText.layoutParams as LayoutParams
                         lpCIV.leftMargin = 0
                         lpCIV.rightMargin = 40
-                        my_textview.layoutParams = lpCIV
+                        binding.menuText.layoutParams = lpCIV
                     }
                 }
             }
