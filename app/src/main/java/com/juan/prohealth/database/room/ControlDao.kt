3ababb2e2456e275aacb25a80dbc8f9bbe066e86
ArmanMac <Arman.agora@gmail.com>
2021-06-11 09:37:42 +0200

    Added components for use Database with Room
    -Se han añadido Entities, Db, Dao's y clases de configuración para manipular datos con Room.

---
+++ app/src/main/java/com/juan/prohealth/database/room/ControlDao.kt
@@ -0,0 +1,35 @@
+package com.juan.prohealth.database.room
+
+import androidx.room.*
+
+@Dao
+interface ControlDao {
+
+    @Query("update control set medicated = 1 where medicated = 0 and execution_date < date() and user_id = :idUser")
+    suspend fun updateStateToCloseControls(idUser: Int)
+
+    @Query("select count(*) from control where date() >= execution_date and medicated = 0 and user_id = :idUser")
+    suspend fun getNumberPendingControls(idUser: Int): Int
+
+    @Query("select count(*) from control where execution_date = date() and medicated = 0 and user_id = :idUser")
+    suspend fun getNumberOfControlsToday(idUser: Int): Int
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: Control)
+
+    @Query("SELECT resource from control where user_id = :idUser and execution_date = date() and cast(strftime('%H', datetime()) as int) >= :hour and cast(strftime('%M', datetime()) as int) >= :minute")
+    suspend fun checkPendingControlToday(hour: Int, minute: Int, idUser: Int): String
+
+    @Query("update control set medicated = :status where execution_date = date() and medicated = 0 and user_id = :idUser")
+    suspend fun updateCurrentControl(idUser: Int, status: Boolean)
+
+    // Coge los dias del ultimo grupo de control
+    @Query("SELECT * FROM control WHERE user_id = :idUser AND medicated = :medicated AND group_control = (SELECT group_control FROM control WHERE user_id = :idUser order by id limit 1)")
+    suspend fun getActiveControlList(idUser: Int, medicated: Boolean): List<Control>
+
+    @Query("DELETE FROM control WHERE group_control = (SELECT group_control FROM control WHERE user_id = :idUser order by id limit 1)")
+    suspend fun deleteLastControlGroup(idUser: Int)
+
+    @Query("SELECT distinct(blood) FROM control order by id desc limit :limit")
+    suspend fun getLastBloodValues(limit: Int = 10): Array<Float>
+}
---
+++ app/src/main/java/com/juan/prohealth/database/room/User.kt
@@ -0,0 +1,27 @@
+package com.juan.prohealth.database.room
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "user", indices = [Index(value = ["id_server"], unique = true)])
+@Parcelize
+data class User(
+    // Probar generacion usuarios
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    val name: String = "",
+    val blood: Float = 0f,
+    val level: Int = 0,
+    @ColumnInfo(name = "hour_alarm")
+    val hourAlarm: Int = 0,
+    @ColumnInfo(name = "minute_alarm")
+    val minuteAlarm: Int = 0,
+    @ColumnInfo(name = "id_server")
+    val idServer: Int = 0,
+    @ColumnInfo(name = "state_logging")
+    val stateLogging: Boolean = false
+): Parcelable
