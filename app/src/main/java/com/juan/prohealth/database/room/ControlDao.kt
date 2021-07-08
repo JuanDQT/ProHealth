95bc523deaa25c0cc00221355967ecb7aba4ba6c
ArmanMac <Arman.agora@gmail.com>
2021-07-08 21:49:40 +0200

    Update Dao  for getControlToday by Date()
    Se han creado metodos dao para obtener en una consulta un numero o un control en funcion de execute_date y date(). Para conseguir esto previamente hemos configurado las fechas generadas en formato UTC que sean compatibles con las que generamos en SQLite ya que sino, no serán compatibles.
    Tambien hemos cambiado los valores BOOLEAN por INT ya que en SQLite no convierte "magicamente" nuestros Boolean's en 1 o 2.

--- app/src/main/java/com/juan/prohealth/database/room/ControlDao.kt
+++ app/src/main/java/com/juan/prohealth/database/room/ControlDao.kt
@@ -6,6 +6,12 @@ import java.util.*
 @Dao
 interface ControlDao {
 
+    @Update
+    suspend fun update(control: Control)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: Control)
+
     @Query("update control set medicated = 1 where medicated = 0 and execution_date < date() and user_id = :idUser")
     suspend fun updateStateToCloseControls(idUser: Int)
 
@@ -15,18 +21,12 @@ interface ControlDao {
     @Query("select count(*) from control where execution_date = date() and medicated = 0 and user_id = :idUser")
     suspend fun getNumberOfControlsToday(idUser: Int): Int
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(user: Control)
-
-    @Query("SELECT resource from control where user_id = :idUser and execution_date = date() and cast(strftime('%H', datetime()) as int) >= :hour and cast(strftime('%M', datetime()) as int) >= :minute")
-    suspend fun checkPendingControlToday(hour: Int, minute: Int, idUser: Int): String
-
     @Query("update control set medicated = :status where execution_date = date() and medicated = 0 and user_id = :idUser")
     suspend fun updateCurrentControl(idUser: Int, status: Boolean)
 
     // Coge los dias del ultimo grupo de control
-    @Query("SELECT * FROM control WHERE user_id = :idUser AND medicated = :medicated AND group_control = (SELECT group_control FROM control WHERE user_id = :idUser order by id limit 1)")
-    suspend fun getActiveControlList(idUser: Int, medicated: Boolean): List<Control>
+    @Query("SELECT * FROM control WHERE user_id = :idUser AND group_control = (SELECT group_control FROM control WHERE user_id = :idUser order by id limit 1)" )
+    suspend fun getActiveControlList(idUser: Int): List<Control>
 
     @Query("DELETE FROM control WHERE group_control = (SELECT group_control FROM control WHERE user_id = :idUser order by id limit 1)")
     suspend fun deleteLastControlGroup(idUser: Int)
@@ -34,12 +34,19 @@ interface ControlDao {
     @Query("SELECT distinct(blood) FROM control order by id desc limit :limit")
     suspend fun getLastBloodValues(limit: Int = 10): Array<Float>
 
+    @Query("SELECT MAX(group_control) + 1 FROM control")
+    suspend fun generateNewIdGroupFromDb(): Int?
+
     @Query("SELECT * FROM control")
     suspend fun getAllControl(): List<Control>
 
     @Query("SELECT * FROM control WHERE execution_date = :date")
     suspend fun getControlByDate(date:Date): Control
 
-    @Update
-    suspend fun update(control: Control)
+    @Query("SELECT COUNT(*) FROM control WHERE medicated = :isPending and  (execution_date = (CAST(strftime('%s', date())  AS  int) * 1000)) AND user_id = (SELECT id FROM user WHERE state_logging = 1)")
+    suspend fun getNumberControlTodayWithSQuery(isPending: Int): Int
+
+    @Query("SELECT COUNT(*) FROM control WHERE medicated = :isPending and  (execution_date = (CAST(strftime('%s', date())  AS  int) * 1000)) AND user_id = (SELECT id FROM user WHERE state_logging = 1)")
+    suspend fun getControlByDate(isPending: Int): Control?
+
 }
